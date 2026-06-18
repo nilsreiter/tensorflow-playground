800cb639a05a5f155f0412e043c6176aacec7fa0
Nils Reiter <nils.reiter@uni-koeln.de>
2026-06-18 09:36:48 +0200

    Removed Google Analytics

--- src/playground.ts
+++ src/playground.ts
@@ -1099,17 +1099,9 @@ function userHasInteracted() {
   if (state.tutorial != null && state.tutorial !== '') {
     page = `/v/tutorials/${state.tutorial}`;
   }
-  ga('set', 'page', page);
-  ga('send', 'pageview', {'sessionControl': 'start'});
 }
 
 function simulationStarted() {
-  ga('send', {
-    hitType: 'event',
-    eventCategory: 'Starting Simulation',
-    eventAction: parametersChanged ? 'changed' : 'unchanged',
-    eventLabel: state.tutorial == null ? '' : state.tutorial
-  });
   parametersChanged = false;
 }
 
